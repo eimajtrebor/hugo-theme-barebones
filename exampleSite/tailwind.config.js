619056c65efda766b0d8a584e24d477a7bba1e49
Jim McCormick <jim.mccormick@sbvonline.co.uk>
2021-08-21 18:20:31 +0100

    add fonts partial

--- exampleSite/tailwind.config.js
+++ exampleSite/tailwind.config.js
@@ -1,8 +1,14 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
       extend: {
+          fontFamily: {
+              mono: ["'Courier Prime'", ...defaultTheme.fontFamily.mono],
+              sans: ["'Open Sans'", ...defaultTheme.fontFamily.sans]
+          }
       },
   },
   variants: {
